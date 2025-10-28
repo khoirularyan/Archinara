777f260e168a145d5174f9bd0a3946a16ba80707
Khoirul Aryan <khoirularyan123@gmail.com>
2025-10-28 16:09:53 +0700

    pisah homepage tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,5 @@
-"use client";
-
-import { type RefObject } from "react";
-import { useScrollAnimations, useCarousel } from "@/hooks";
-import { Header, Footer } from "@/components/layout";
-import { Hero, Projects, About, CTA, Testimonials, FAQ } from "@/components/sections";
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
-  useScrollAnimations();
-  const carouselRef = useCarousel();
-
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Projects />
-      <About />
-      <CTA />
-      <Testimonials carouselRef={carouselRef as RefObject<HTMLDivElement>} />
-      <FAQ />
-      <Footer />
-    </>
-  );
+  return <HomePage />;
 }
